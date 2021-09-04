8907fc365ac272d72d5f57f496e587e16ed4c3ce
maackey <maackey@gmail.com>
2021-09-04 19:58:01 -0400

    add self-righting to flipped car

--- scenes/Car.gd
+++ scenes/Car.gd
@@ -1,21 +1,20 @@
 extends Spatial
 
+onready var body = $CarBody
+const turn_speed = 8
+const acceleration_forward = 15
+const acceleration_sideways = 10
+const flip_force = 20
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	$CarBody.add_torque(Vector3.UP*8*(Input.get_action_strength("steer_left")-Input.get_action_strength("steer_right")))
-	$CarBody.add_force($CarBody.global_transform.basis.xform(Vector3.FORWARD*15*(Input.get_action_strength("move_forward")-Input.get_action_strength("move_backward"))),Vector3.ZERO)
-	$CarBody.add_force($CarBody.global_transform.basis.xform(Vector3.LEFT*10*(Input.get_action_strength("move_left")-Input.get_action_strength("move_right"))),Vector3.ZERO)
-
-func _input(e: InputEvent) -> void:
-	pass
+	var rot_input = Input.get_action_strength("steer_left") - Input.get_action_strength("steer_right")
+	var z_input = Input.get_action_strength("move_forward") - Input.get_action_strength("move_backward")
+	var x_input = Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
+	var flipped = body.global_transform.basis.y.dot(Vector3.UP)
+	
+	if flipped < -0.5:
+		body.add_torque(body.transform.basis.z * flip_force * flipped)
+		
+	body.add_torque(Vector3.UP * turn_speed * rot_input)
+	body.add_force(body.global_transform.basis.xform(Vector3.FORWARD * acceleration_forward * z_input), Vector3.ZERO)
+	body.add_force(body.global_transform.basis.xform(Vector3.LEFT * acceleration_sideways * x_input), Vector3.ZERO)
