f5401ca5ccedcd1730bf6c247cbe76abcbe0887c
RealOfficialTurf <59943769+RealOfficialTurf@users.noreply.github.com>
2021-09-05 07:21:53 +0700

    Minor adjustment to car self-righting algorithm

--- scenes/Car.gd
+++ scenes/Car.gd
@@ -8,13 +8,16 @@ const flip_force = 20
 
 func _physics_process(delta: float) -> void:
 	var rot_input = Input.get_action_strength("steer_left") - Input.get_action_strength("steer_right")
-	var z_input = Input.get_action_strength("move_forward") - Input.get_action_strength("move_backward")
-	var x_input = Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
+	#Since the car faces toward -Z direction, forward is negative Z.
+	var z_input = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
+	#Same goes for X axis.
+	var x_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var flipped = body.global_transform.basis.y.dot(Vector3.UP)
 	
 	if flipped < -0.5:
-		body.add_torque(body.transform.basis.z * flip_force * flipped)
+		#Use the cross product to correctly flip the car so that it stays upright.
+		body.add_torque(body.global_transform.basis.y.cross(Vector3.UP).normalized() * flip_force * -flipped)
 		
-	body.add_torque(Vector3.UP * turn_speed * rot_input)
-	body.add_force(body.global_transform.basis.xform(Vector3.FORWARD * acceleration_forward * z_input), Vector3.ZERO)
-	body.add_force(body.global_transform.basis.xform(Vector3.LEFT * acceleration_sideways * x_input), Vector3.ZERO)
+	body.add_torque(body.global_transform.basis.y * turn_speed * rot_input)
+	body.add_force(body.global_transform.basis.z * acceleration_forward * z_input, Vector3.ZERO)
+	body.add_force(body.global_transform.basis.x * acceleration_sideways * x_input, Vector3.ZERO)
